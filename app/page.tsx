8d6a48fb05cc073bfd294e7605774ac888cb01af
Thibault Weytens <thibault.weytensm@student.howest.be>
2025-04-22 08:24:58 +0200

    commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import SkillsPage from "../_components/SkillsPage";
+import Hero from "../_components/Hero";
+import Navbar from "../_components/Navbar";
+import ProjectsPage from "../_components/ProjectsPage";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <SkillsPage />
+      <ProjectsPage />
+    </div>
+  );
+}
